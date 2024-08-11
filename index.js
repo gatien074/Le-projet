d56135a3afb51c429d7ce8ff05a9ba1b95bdc2ce
gatien074 <ngouessygatien@gmail.com>
2024-08-11 19:00:22 +0100

    version3

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const mybutton = document.getElementById('myButton');
+function typeEffect(element, need) {
+  const text = element.innerHTML;
+  element.innerHTML = "";
+  
+  var i = 0;
+  var timer = setInterval(function() {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, need);
+}
 
-mybutton.addEventListener('click', () => {
-  alert('Hello, World!');
-});
-console.log('Button clicked!');
 
+// application
+var need = 90;
+var h1 = document.querySelector('h1');
+var p = document.querySelector('p');
+var delay = h1.innerHTML.length * need + need;
+
+// type affect to header
+ typeEffect(h1, need);
+
+
+// type affect to body
+setTimeout(function(){
+  p.style.display = "inline-block";
+  typeEffect(p, need);
+}, delay);
